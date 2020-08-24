cba045bbf797235e6dba8a98354d93ee6c18efcb
Jérôme Crepin <jerome.crepin@outlook.com>
2020-08-24 18:53:41 +0200

    Update of the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,67 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceButton = rnd();
+  int rightDiceButton = rnd();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceButton = rnd();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceButton.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceButton = rnd();
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceButton.png'),
+              ),
+            ),
+          ],
+        ),
+        FlatButton(
+          onPressed: () {
+            setState(() {
+              leftDiceButton = rnd();
+              rightDiceButton = rnd();
+            });
+          },
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          child: Text(
+            'Random both Dice',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
   }
+}
+
+int rnd() {
+  int rnd = Random().nextInt(6) + 1;
+  return rnd;
 }
